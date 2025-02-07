bb51968320a3445cf1f063cf251ba0a3cea9698b
Cristian Andrés Prieto Jiménez <44248688+caprietoj@users.noreply.github.com>
2025-02-07 13:46:52 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 set -e
 
+# Verifica si no existe el archivo .env, lo copia desde .env.example
+if [ ! -f .env ]; then
+    if [ -f .env.example ]; then
+        echo ".env no encontrado. Copiando .env.example a .env..."
+        cp .env.example .env
+    else
+        echo "Error: No se encontró .env ni .env.example."
+        exit 1
+    fi
+fi
+
 # Si no existe el directorio vendor, ejecutar composer install
 if [ ! -d vendor ]; then
     echo "No se encontró el directorio vendor. Ejecutando composer install..."
